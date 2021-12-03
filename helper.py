f9c4bcabaaaaf6cf82ce0c4bb64b8b74a38aaea5
benoit <bbequignon@gmail.com>
2021-12-03 16:40:49 +0100

    creating modules

---
+++ helper.py
@@ -0,0 +1,28 @@
+def days_to_units(number_of_days, conversion_unit):
+    if conversion_unit == "hours":
+        return f"{number_of_days} days are {number_of_days * 24} hours"
+    elif conversion_unit == "minutes":
+        return f"{number_of_days} days are {number_of_days * 24 * 60} minutes"
+    elif conversion_unit == "seconds":
+        return f"{number_of_days} days are {number_of_days * 24 * 60 * 60} seconds"
+    else:
+        return "bad unit entered, please enter hours, minutes or seconds"
+
+
+def validate_and_execute(days_and_unit_dictionary):
+    try:
+        user_input_number = int(days_and_unit_dictionary["days"])
+
+        # we want to do conversion only for positive integers
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("you entered a 0, please enter a valid positive number")
+        else:
+            print("you entered a negative number, no conversion for you")
+    except ValueError:
+        print("your input is not a valid number, don't ruin my program!")
+
+
+user_input_message = "Enter number of days and conversion unit with : separator \n"
